cbabe295252c81fcbc8ea8ba7764d9774ad8f8aa
spjcollege <2023.sumanyu.joshi@ves.ac.in>
2025-08-11 22:34:50 +0530

    Just started actually working.

--- task_manager/src/main.jsx
+++ task_manager/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
