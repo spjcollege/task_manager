92f1e0c4b85fdfbc2ba83fc30fdc114f3c6ececf
Sumanyu <2023.sumanyu.joshi@ves.ac.in>
2025-09-25 01:14:30 +0530

    Lab 4 complete code part 1

--- task_manager/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // ✅ required
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
